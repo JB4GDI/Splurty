31b8ca571e07c46c957ce88d78e32b37093f849b
Jaime <JB4GDI@gmail.com>
2015-09-28 22:12:54 +0000

    fixed validation on form

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ActiveRecord::Base
 	# :presence => true makes the 
 	validates :saying, :presence => true, :length => { :maximum => 140, :minimum => 3 }
+	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3 }
 end
