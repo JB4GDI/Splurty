6b4f69e5ecc9597e9f55c8959ff6b3d6b3e5ab74
Jaime <JB4GDI@gmail.com>
2015-09-28 20:22:51 +0000

    Form now lets you create a quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,23 @@
 class QuotesController < ApplicationController
 	def index
-		@quote = Quote.first
+		@quote = Quote.order("RANDOM()").first
 	end
 
 	def new
 		@quote = Quote.new
+	end
+
+	# Create a new quote using the parameters given in the form
+	def create
+		Quote.create(quote_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def quote_params
+		#permit - only allows these params to be saved
+		params.require(:quote).permit(:saying, :author)
 	end
 
 end
